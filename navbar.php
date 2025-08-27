0c34240c5fa488fa186e5343505427e348d1d9bc
PRINCE ALVIN YUSUF <princealvinyusuf@gmail.com>
2025-08-27 20:49:14 +0700

    Add forecasting job seekers

--- navbar.php
+++ navbar.php
@@ -1,83 +1,88 @@
 <!-- Navigation Bar -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-	<div class="container">
-		<a class="navbar-brand" href="index.php"><i class="bi bi-briefcase me-2"></i>Job Admin</a>
-		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-		<div class="collapse navbar-collapse" id="navbarNav">
-			<ul class="navbar-nav ms-auto">
-				<li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle" href="#" id="dashboardDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-						Dashboard
-						</a>
-					<ul class="dropdown-menu" aria-labelledby="dashboardDropdown">
-						<li><a class="dropdown-item" href="dashboard_jobs.php">Dashboard Jobs</a></li>
-						<li><a class="dropdown-item" href="dashboard_job_seekers.php">Dashboard Job Seekers</a></li>
-					</ul>
-				</li>
-				<li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle" href="#" id="masterDataDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-						Master Data
-					</a>
-					<ul class="dropdown-menu" aria-labelledby="masterDataDropdown">
-						<li><a class="dropdown-item" href="jobs.php">Jobs</a></li>
-						<li><a class="dropdown-item" href="job_seekers.php">Job Seekers</a></li>
-					</ul>
-				</li>
-				<li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle" href="#" id="cleansingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-						Cleansing
-					</a>
-					<ul class="dropdown-menu" aria-labelledby="cleansingDropdown">
-						<li><a class="dropdown-item" href="cleansing_snaphunt.php">Snaphunt</a></li>
-						<li><a class="dropdown-item" href="cleansing_makaryo.php">Makaryo</a></li>
-					</ul>
-				</li>
-				<li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle" href="#" id="settingsDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-						Settings
-					</a>
-					<ul class="dropdown-menu" aria-labelledby="settingsDropdown">
-						<li><a class="dropdown-item" href="chart_settings.php">Chart Settings</a></li>
-						<li><a class="dropdown-item" href="contribution_settings.php">Contribution Settings</a></li>
-						<li><a class="dropdown-item" href="information_settings.php">Information Settings</a></li>
-						<li><a class="dropdown-item" href="news_settings.php">News Settings</a></li>
-						<li><a class="dropdown-item" href="services_settings.php">Services Settings</a></li>
-						<li><a class="dropdown-item" href="statistics_settings.php">Statistics Settings</a></li>
-						<li><a class="dropdown-item" href="testimonials_settings.php">Testimonial Settings</a></li>
-						<li><a class="dropdown-item" href="top_list_settings.php">Top List Settings</a></li>
-						<li><hr class="dropdown-divider"></li>
-						<li><a class="dropdown-item" href="agenda_settings.php">Agenda Settings</a></li>
-						<li><a class="dropdown-item" href="job_fair_settings.php">Job Fair Settings</a></li>
-						<li><a class="dropdown-item" href="virtual_karir_service_settings.php">Virtual Karir Service Settings</a></li>
-						<li><hr class="dropdown-divider"></li>
-						<li><a class="dropdown-item" href="mitra_kerja_settings.php">Mitra Kerja Settings</a></li>
-						<li><a class="dropdown-item" href="partnership_type_settings.php">Partnership Type Settings</a></li>
-						<li><a class="dropdown-item" href="kemitraan_submission.php">Mitra Kerja Submission</a></li>
-						<li><a class="dropdown-item" href="kemitraan_booked.php">Kemitraan Booked</a></li>
-						<li><a class="dropdown-item" href="pasker_room_settings.php">Pasker Room Settings</a></li>
-						<li><hr class="dropdown-divider"></li>
-						<li><a class="dropdown-item" href="access_control.php">Access Control</a></li>
-						<li><hr class="dropdown-divider"></li>
+    <div class="container">
+        <a class="navbar-brand" href="index.php"><i class="bi bi-briefcase me-2"></i>Job Admin</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="dashboardDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Dashboard
+                        </a>
+                    <ul class="dropdown-menu" aria-labelledby="dashboardDropdown">
+                        <li><a class="dropdown-item" href="dashboard_jobs.php">Dashboard Jobs</a></li>
+                        <li><a class="dropdown-item" href="dashboard_job_seekers.php">Dashboard Job Seekers</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="masterDataDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Master Data
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="masterDataDropdown">
+                        <li><a class="dropdown-item" href="jobs.php">Jobs</a></li>
+                        <li><a class="dropdown-item" href="job_seekers.php">Job Seekers</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="cleansingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Cleansing
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="cleansingDropdown">
+                        <li><a class="dropdown-item" href="cleansing_snaphunt.php">Snaphunt</a></li>
+                        <li><a class="dropdown-item" href="cleansing_makaryo.php">Makaryo</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="forecastingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Forecasting
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="forecastingDropdown">
+                        <li><a class="dropdown-item" href="forecasting_job_seekers.php">Forecasting Job Seekers</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="settingsDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Settings
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="settingsDropdown">
+                        <li><a class="dropdown-item" href="chart_settings.php">Chart Settings</a></li>
+                        <li><a class="dropdown-item" href="contribution_settings.php">Contribution Settings</a></li>
+                        <li><a class="dropdown-item" href="information_settings.php">Information Settings</a></li>
+                        <li><a class="dropdown-item" href="news_settings.php">News Settings</a></li>
+                        <li><a class="dropdown-item" href="services_settings.php">Services Settings</a></li>
+                        <li><a class="dropdown-item" href="statistics_settings.php">Statistics Settings</a></li>
+                        <li><a class="dropdown-item" href="testimonials_settings.php">Testimonial Settings</a></li>
+                        <li><a class="dropdown-item" href="top_list_settings.php">Top List Settings</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="agenda_settings.php">Agenda Settings</a></li>
+                        <li><a class="dropdown-item" href="job_fair_settings.php">Job Fair Settings</a></li>
+                        <li><a class="dropdown-item" href="virtual_karir_service_settings.php">Virtual Karir Service Settings</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="mitra_kerja_settings.php">Mitra Kerja Settings</a></li>
+                        <li><a class="dropdown-item" href="partnership_type_settings.php">Partnership Type Settings</a></li>
+                        <li><a class="dropdown-item" href="kemitraan_submission.php">Mitra Kerja Submission</a></li>
+                        <li><a class="dropdown-item" href="kemitraan_booked.php">Kemitraan Booked</a></li>
+                        <li><a class="dropdown-item" href="pasker_room_settings.php">Pasker Room Settings</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="access_control.php">Access Control</a></li>
+                        <li><hr class="dropdown-divider"></li>
                         <li><a class="dropdown-item" href="broadcast.php">Broadcast</a></li>
                         <li><hr class="dropdown-divider"></li>
                         <li><a class="dropdown-item" href="database_contact.php">Database Contact</a></li>
                         <li><hr class="dropdown-divider"></li>
-						<li><a class="dropdown-item" href="cron_settings.php">Other Settings</a></li>
-					</ul>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="extensions.php">Extensions</a>
-				</li>
-				<!-- <li class="nav-item">
-					<a class="nav-link" href="jobstreet_scrapping.php">Jobstreet Scrapping</a>
-				</li> -->
-				<li class="nav-item">
-					<a class="nav-link" href="logout.php"><i class="bi bi-box-arrow-right me-1"></i>Logout</a>
-				</li>
-			</ul>
-		</div>
-	</div>
+                        <li><a class="dropdown-item" href="cron_settings.php">Other Settings</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="extensions.php">Extensions</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="logout.php"><i class="bi bi-box-arrow-right me-1"></i>Logout</a>
+                </li>
+            </ul>
+        </div>
+    </div>
 </nav>
 <!-- End Navigation Bar --> 
