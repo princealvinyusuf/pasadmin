cc0efd027bca591da81aeadeba3bd99202c4431f
PRINCE ALVIN YUSUF <princealvinyusuf@gmail.com>
2025-10-04 13:35:46 +0700

    Added Download Button

--- naker_award_initial_assessment.php
+++ naker_award_initial_assessment.php
@@ -115,6 +115,9 @@ $resultRow = null; $message = '';
         <div class="d-flex gap-2">
             <a class="btn btn-outline-secondary" href="naker_award_stage1_shortlisted_c.php">View Stage 1 Shortlisted C</a>
             <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#bulkImportModal"><i class="bi bi-file-earmark-excel"></i> Import Excel</button>
+            <a class="btn btn-outline-primary" href="https://paskerid.kemnaker.go.id/paskerid/public/downloads/TemplateBulking_Initial_Assessment.xlsx" target="_blank" rel="noopener">
+                <i class="bi bi-download"></i> Download Template
+            </a>
         </div>
     </div>
 
