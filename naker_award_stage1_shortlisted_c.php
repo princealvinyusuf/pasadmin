91fe548fc298d9a5320f36185631f30192e0d3e3
Prince Alvin Yusuf <princealvinyusuf@gmail.com>
2025-10-10 16:25:28 +0700

    Update logic

--- naker_award_stage1_shortlisted_c.php
+++ naker_award_stage1_shortlisted_c.php
@@ -131,6 +131,7 @@ if ($useCriteria) {
         . "END";
 
     $sqlList = 'SELECT *, ' . $tierCase . ' AS tier FROM naker_award_assessments '
+        . ' WHERE CAST(IFNULL(NULLIF(total_indeks,\'\'),\'0\') AS DECIMAL(15,4)) >= 60 '
         . ' ORDER BY tier ASC, CAST(IFNULL(NULLIF(total_indeks,\'\'),\'0\') AS DECIMAL(15,4)) DESC, '
         . ' CAST(IFNULL(NULLIF(postings_count,\'\'),\'0\') AS DECIMAL(15,4)) DESC, '
         . ' CAST(IFNULL(NULLIF(quota_count,\'\'),\'0\') AS DECIMAL(15,4)) DESC '
