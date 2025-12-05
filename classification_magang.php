08a379d17f35503bf3dbad3bb1e5b56d09f53f33
Achmadky04 <achmadky555@gmail.com>
2025-12-05 11:07:45 +0700

    fixing error 500

--- classification_magang.php
+++ classification_magang.php
@@ -1,5 +1,9 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require __DIR__ . '/../vendor/autoload.php'; // ← coba pakai ../ dulu
 
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
