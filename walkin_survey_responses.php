9e17841708d3e0e885dffdbe96e4f50999cbe91b
Prince Alvin Yusuf <princealvinyusuf@gmail.com>
2026-07-08 09:46:17 +0700

    fixing bug

--- walkin_survey_responses.php
+++ walkin_survey_responses.php
@@ -680,7 +680,7 @@ document.addEventListener('DOMContentLoaded', function () {
         btnConvert.addEventListener('click', function () {
             downloadExcel({ withConvertedScale: true });
         });
-    });
+    }
 });
 </script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
